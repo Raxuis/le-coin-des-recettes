e7ca7bbe1f6400df1faa4bb3a355fae5c7558555
Raxuis <raclot.raphael@gmail.com>
2024-10-27 15:02:05 +0100

    feat(read more): add dish recipe with slugTitle route

---
+++ pages/recipe/dish/[recipeTitle]/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const recipeTitle = route.params.recipeTitle;
+</script>
+
+<template>
+  <div>
+    <h1>Recipe: {{ recipeTitle }}</h1>
+  </div>
+</template>
