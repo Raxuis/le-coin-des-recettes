0273a06688828550875d4a64f53fbb2eb9d3d316
Raxuis <raclot.raphael@gmail.com>
2024-10-27 11:15:10 +0100

    feat(landing page): add mapping over recipes

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 const recipe = ref('')
+import { useFetch } from '#app';
+import type { Recipes } from '@prisma/client';
+const { data, status } = useFetch<Recipes[]>('/api/recipes');
+if (data.value) console.log(data.value);
 </script>
 
 <template>
@@ -17,6 +21,25 @@ const recipe = ref('')
         square
         variant="solid"
         />
+    </div>
+    <div>
+      <p v-if="status === 'pending'">
+        Loading...
+      </p>
+      <div v-else-if="status === 'error'">
+      <p class="text-lg">Error</p>
+    </div>
+    <div v-else>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+        <UCard v-for="recipe in data?.slice(0, 30)" :key="recipe.title">
+  <template #header>
+    <div class="flex flex-col space-y-2 items-center justify-center">
+      <p class="text-xl">{{ recipe.title }}</p>
+    </div>
+  </template>
+</UCard>
+      </div>
+    </div>
     </div>
   </div>
 </template>
