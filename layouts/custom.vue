cc1d85ce3d6eac61615355dc70c190fe56ae0ce5
Raxuis <raclot.raphael@gmail.com>
2024-10-28 21:09:05 +0100

    feat(dark mode): add dark mode toggler

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
 const links = [{
   label: 'À propos',
   to: '/about'
@@ -13,13 +23,23 @@ const links = [{
     <nav class="flex justify-between items-center my-4">
       <a href="/" class="flex gap-2 items-center">
         <NuxtImg src="/icon.png" class="size-10" />
-        <p class="text-2xl text-masala-900 hover:text-persian-red-800">Le Coin des Recettes</p>
+        <p class="text-2xl text-masala-900 hover:text-persian-red-800 dark:text-white transition-colors">Le Coin des Recettes</p>
       </a>
-      <ul class="flex gap-2">
-        <li v-for="link in links" :key="link.label" class="list-none text-masala-900 hover:text-persian-red-800">
+      <ul class="flex gap-2 items-center">
+        <li v-for="link in links" :key="link.label" class="list-none text-masala-900 hover:text-persian-red-800 dark:text-white dark:hover:text-persian-red-400 transition-colors">
           <NuxtLink :to=link.to>
             {{ link.label }}
           </NuxtLink>
+        </li>
+        <li>
+          <UButton
+            :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+            color="gray"
+            variant="ghost"
+            aria-label="Theme"
+            @click="isDark = !isDark"
+            class="hover:text-persian-red-800 dark:hover:text-persian-red-400 transition-colors"
+            />
         </li>
       </ul>
     </nav>
