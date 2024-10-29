a776671e34156a3ae74bcd35b6123732cd01005a
Raxuis <raclot.raphael@gmail.com>
2024-10-29 20:45:50 +0100

    feat(index): add user name in welcome page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-const recipe = ref('')
-import { useFetch } from '#app';
-import type { Recipes } from '@prisma/client';
-const { data, status } = useFetch<Recipes[]>('/api/recipes');
+const { data } = useAuth()
 </script>
 
 <template>
   <div class="mt-10 font-lora space-y-4">
     <div>
-      <p>Bienvenue sur <span class="font-semibold">Le Coin des Recettes !</span></p>
+      <p>Bienvenue {{ data?.user?.name }} sur <span class="font-semibold">Le Coin des Recettes !</span></p>
       <p>Maintenant, cherchez une recette qui vous plairait.</p>
     </div>
   </div>
