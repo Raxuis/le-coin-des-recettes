962de2d40849ab5ec980958c63ff3e879a393d17
Raxuis <raclot.raphael@gmail.com>
2024-10-31 16:37:58 +0100

    fix(recipe/dish): fix issue when checking data.people value

--- pages/recipe/dish/[slug]/index.vue
+++ pages/recipe/dish/[slug]/index.vue
@@ -34,7 +34,7 @@ const { data, status } = useFetch<Recipes>('/api/recipe', {
           />
         <p class="text-xl">{{ data?.title }}</p>
       </div>
-      <p class="text-sm" v-if="data?.people">Recipe for : {{ data.people }}</p>
+      <p class="text-sm" v-if="data?.people && data?.people !== 0">Recipe for : {{ data.people }}</p>
       <p class="text-lg">Étapes :</p>
       <ol class="list-decimal list-inside space-y-1">
         <li v-for="step in data?.steps" :key="step">
