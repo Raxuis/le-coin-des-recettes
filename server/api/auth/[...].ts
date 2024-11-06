d4711f8476d467463e4ffe98684688be524c534d
Raxuis <raclot.raphael@gmail.com>
2024-11-06 19:39:58 +0100

    feat(sign in): add user insertion when signing in

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,15 +1,50 @@
 import { NuxtAuthHandler } from '#auth'
-
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 export default NuxtAuthHandler({
   secret: useRuntimeConfig().authSecret,
   providers: [
-    // @ts-expect-error Using .default here for it to work during SSR.
+    //@ts-ignore  Using .default for SSR compatibility
     GoogleProvider.default({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log(user);
+      try {
+        let existingUser = await prisma.user.findUnique({
+          where: { email: user.email || '' },
+        })
+
+        if (!existingUser) {
+          existingUser = await prisma.user.create({
+            data: {
+              email: user.email || '',
+              name: user.name || '',
+              image: user.image || '',
+            },
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.error("Error during sign-in:", error)
+        return false
+      }
+    },
+    async session({ session, user }) {
+      // session.user = {
+      //   name: user.name || null,
+      //   email: user.email || null,
+      //   image: user.image || null,
+      // };
+      // session.expires = new Date(session.expires).toISOString();
+      return session;
+    },
+  }
 })
