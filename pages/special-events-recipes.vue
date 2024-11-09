bc7aa030d297c921ee042ddb6856e21db9707cda
Raxuis <raclot.raphael@gmail.com>
2024-11-09 21:32:13 +0100

    feat(specialEvents): add select for special event

--- pages/special-events-recipes.vue
+++ pages/special-events-recipes.vue
@@ -1,28 +1,36 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted } from 'vue';
+import { reactive } from 'vue';
 import type { FormSubmitEvent } from '#ui/types'
 import { z } from 'zod';
 import { searchSpecialEventsRecipes } from '../validation/schemas';
-import type { Recipes } from '@prisma/client';
+import { SpecialEvents, type Recipes } from '@prisma/client';
+
+console.log(Object.values(SpecialEvents) as [SpecialEvents, ...SpecialEvents[]])
 
 type Schema = z.output<typeof searchSpecialEventsRecipes>
 
 const state = reactive({
-  eventType: "",
+  eventType: undefined,
 })
 
-const specialEventSelected = ref<string>("");
 const { data } = useFetch<Recipes[]>('/api/get-special-events');
 
-
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  console.log(event.data)
+  console.log(event.data);
 }
 </script>
 
 <template>
   <UForm :schema="searchSpecialEventsRecipes" :state="state" class="space-y-4" @submit="onSubmit">
-    <USelectMenu label="Choisissez un évènement" name="eventType" :options="data?.map((event) => ({ label: event.specialEvent, value: event.specialEvent }))" placeholder="Sélectionnez un événement" v-model="specialEventSelected" />
+    <UFormGroup label="Type d'évènement" name="eventType">
+      <USelectMenu
+      label="Choisissez un évènement"
+      name="eventType"
+      :options="data?.map((event) => event.specialEvent).filter((event) => event !== null)"
+      placeholder="Sélectionnez un événement"
+      v-model="state.eventType"
+    />
+    </UFormGroup>
     <UButton type="submit">
       Submit
     </UButton>
