08f461ebeba34dfd67a07dcc92e1d0b3cdfb5be6
Raxuis <raclot.raphael@gmail.com>
2024-11-13 21:48:35 +0100

    fix(zod schema): fix issue with slug being only of type string

--- validation/schemas.ts
+++ validation/schemas.ts
@@ -1,53 +1,29 @@
-import { z } from "zod";
-import { budget, category, difficulty, SPECIAL_EVENTS } from "@/constants";
+import {z} from "zod";
+import {budget, category, difficulty, SPECIAL_EVENTS} from "@/constants";
 
 export const searchSpecialEventsRecipes = z.object({
-  eventType: z.enum(SPECIAL_EVENTS, {
-    errorMap: () => ({ message: `L'évènement est obligatoire et doit être une des valeurs : ${SPECIAL_EVENTS.join(", ")}.` }),
-  }),
+    eventType: z.enum(SPECIAL_EVENTS, {
+        errorMap: () => ({message: `L'évènement est obligatoire et doit être une des valeurs : ${SPECIAL_EVENTS.join(", ")}.`}),
+    }),
 });
 
 export const newRecipe = z.object({
-  title: z.string().min(1, {
-    "message": "Le titre est nécessaire."
-  }).max(100, {
-    "message": "Doucement sur le titre..."
-  }).trim(),
-  eventType: z.enum(category, {
-    "message": "La catégorie est nécessaire."
-  }),
-  people: z.number({
-    "message": "Le nombre de personnes est requis."
-  }).min(0, {
-    "message": "Doit être supérieur à 0."
-  }),
-  ingredients: z.string().min(1, {
-    "message": "Au moins un ingrédient est nécessaire."
-  }),
-  steps: z.string().min(1, {
-    "message": "Au moins une étape est nécessaire."
-  }),
-  preparationTime: z.number({
-    "message": "Le temps est requis."
-  }).min(0, {
-    "message": "La temps doit être supérieur à 0."
-  }),
-  restingTime: z.number({
-    "message": "Le temps est requis."
-  }).min(0, {
-    "message": "La temps doit être supérieur à 0."
-  }),
-  cookingTime: z.number({
-    "message": "Le temps est requis."
-  }).min(0, {
-    "message": "La temps doit être supérieur à 0."
-  }),
-  difficulty: z.enum(difficulty, {
-    "message": "Le difficulté est nécessaire."
-  }),
-  budget: z.enum(budget, {
-    "message": "Le budget est nécessaire."
-  }),
-  specialEvent: z.enum(SPECIAL_EVENTS).optional(),
-  slug: z.string().optional(),
+    title: z.string().min(1, {message: "Le titre est nécessaire."}).max(100, {message: "Doucement sur le titre..."}).trim(),
+    eventType: z.enum(category, {message: "La catégorie est nécessaire."}),
+    people: z.number({message: "Le nombre de personnes est requis."}).min(0, {message: "Doit être supérieur à 0."}),
+    ingredients: z.string().min(1, {message: "Au moins un ingrédient est nécessaire."}),
+    steps: z.string().min(1, {message: "Au moins une étape est nécessaire."}),
+    preparationTime: z.number({message: "Le temps est requis."}).min(0, {message: "Le temps doit être supérieur à 0."}),
+    restingTime: z.number({message: "Le temps est requis."}).min(0, {message: "Le temps doit être supérieur à 0."}),
+    cookingTime: z.number({message: "Le temps est requis."}).min(0, {message: "Le temps doit être supérieur à 0."}),
+    difficulty: z.enum(difficulty, {message: "La difficulté est nécessaire."}),
+    budget: z.enum(budget, {message: "Le budget est nécessaire."}),
+    specialEvent: z.enum(SPECIAL_EVENTS).optional(),
+    slug: z
+        .string()
+        .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+            message: "Le slug doit être en minuscules, sans espaces, et peut contenir des tirets (e.g., mon-slug-de-recette).",
+        })
+        .optional(),
 });
+
