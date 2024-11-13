5b1115c793f14c3dc8d5d746596d840b2ff1d83d
Raxuis <raclot.raphael@gmail.com>
2024-11-13 19:28:07 +0100

    style(zod schema): improve error messages

--- validation/schemas.ts
+++ validation/schemas.ts
@@ -8,17 +8,31 @@ export const searchSpecialEventsRecipes = z.object({
 });
 
 export const newRecipe = z.object({
-  title: z.string().min(1).max(100).trim(),
+  title: z.string().min(1, {
+    "message": "Le titre est nécessaire."
+  }).max(100, {
+    "message": "Doucement sur le titre..."
+  }).trim(),
   description: z.string().optional(),
-  eventType: z.enum(category),
+  eventType: z.enum(category, {
+    "message": "La catégorie est nécessaire."
+  }),
   people: z.number().optional(),
-  ingredients: z.string().min(1),
-  steps: z.string().min(1),
+  ingredients: z.string().min(1, {
+    "message": "Au moins un ingrédient est nécessaire."
+  }),
+  steps: z.string().min(1, {
+    "message": "Au moins une étape est nécessaire."
+  }),
   preparationTime: z.number().min(0),
   restingTime: z.number().min(0),
   cookingTime: z.number().min(0),
-  difficulty: z.enum(difficulty),
-  budget: z.enum(budget),
+  difficulty: z.enum(difficulty, {
+    "message": "Le difficulté est nécessaire."
+  }),
+  budget: z.enum(budget, {
+    "message": "Le budget est nécessaire."
+  }),
   specialEvent: z.enum(SPECIAL_EVENTS).optional(),
   slug: z.string().optional(),
 });
