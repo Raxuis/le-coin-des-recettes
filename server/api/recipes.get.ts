f12ef56181d1d27c7db8166997df0013c367f279
Raxuis <raclot.raphael@gmail.com>
2024-11-17 16:22:42 +0100

    fix(recipes api route): removed recipes with creatorId

--- server/api/recipes.get.ts
+++ server/api/recipes.get.ts
@@ -4,7 +4,11 @@ const prisma = new PrismaClient();
 
 
 export default defineEventHandler(async (event) => {
-  const recipes = await prisma.recipes.findMany()
+  const recipes = await prisma.recipes.findMany({
+    where: {
+      creatorId: null,
+    }
+  });
   if (!recipes) throw createError({ statusCode: 500, statusMessage: 'An error occurred try again.' })
   return recipes;
 })
