1567a2fed0f183b7a093d8374929d9d6af465934
Raxuis <raclot.raphael@gmail.com>
2024-12-09 20:22:56 +0100

    test(nuxt config): trying to avoid error with /session

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  googleFonts: {
-    families: {
-      Lora: true,
-      "Playfair Display": true,
-    }
-  },
-  colorMode: {
-    preference: 'light'
-  },
-  auth: {
-    isEnabled: true,
-    disableServerSideAuth: false,
-    baseURL: process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`,
-    provider: {
-      defaultProvider: "google",
-      type: "authjs",
-      trustHost: false,
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+    googleFonts: {
+        families: {
+            Lora: true,
+            "Playfair Display": true,
+        }
     },
-    sessionRefresh: {
-      enablePeriodically: true,
-      enableOnWindowFocus: true,
+    colorMode: {
+        preference: 'light'
     },
     auth: {
-      globalAppMiddleware: true
-    }
-  },
-  modules: [
-    '@vueuse/nuxt',
-    '@nuxt/image',
-    '@nuxtjs/google-fonts',
-    '@nuxt/ui',
-    '@sidebase/nuxt-auth',
-    '@vueuse/motion/nuxt',
-  ]
+        isEnabled: true,
+        disableServerSideAuth: false,
+        baseURL: process.env.BASE_URL ? `${process.env.BASE_URL}/api/auth` : `http://localhost:${process.env.PORT || 3000}/api/auth`,
+        provider: {
+            defaultProvider: "google",
+            type: "authjs",
+            trustHost: false,
+        },
+        sessionRefresh: {
+            enablePeriodically: true,
+            enableOnWindowFocus: true,
+        },
+        auth: {
+            globalAppMiddleware: true
+        }
+    },
+    modules: [
+        '@vueuse/nuxt',
+        '@nuxt/image',
+        '@nuxtjs/google-fonts',
+        '@nuxt/ui',
+        '@sidebase/nuxt-auth',
+        '@vueuse/motion/nuxt',
+    ]
 })
